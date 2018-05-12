12a30308c8ff2e99e24296de96869c5a40c1c052
ironmaniiith <aalekhj2507@gmail.com>
2018-05-12 15:34:39 +0530

    Indentation fix
    tab to spaces

--- cppdsa/binarySearch.cpp
+++ cppdsa/binarySearch.cpp
@@ -2,32 +2,32 @@
 using namespace std;
 
 int binarySearch(vector<int>&A, int key) {
-	int l = A.size();
-	int low = 0, up = l - 1;
-	if (key < A[low] || key > A[up]) return -1;
-	while (low <= up) {
-		int mid = (low + up) / 2;
-		if (A[mid] < key) {
-			low = mid + 1;
-		} else if (A[mid] > key) {
-			up = mid - 1;
-		} else {
-			return mid;
-		}
-	}
-	return -1;
+    int l = A.size();
+    int low = 0, up = l - 1;
+    if (key < A[low] || key > A[up]) return -1;
+    while (low <= up) {
+        int mid = (low + up) / 2;
+        if (A[mid] < key) {
+            low = mid + 1;
+        } else if (A[mid] > key) {
+            up = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
 }
 
 int main(int argc, char const *argv[])
 {
-	int n, key;
-	cin >> n;
-	vector<int> A(n);
-	for (int i = 0; i < n; ++i) {
-		cin >> A[i];
-	}
-	cin >> key;
-	int idx = binarySearch(A, key);
-	cout << idx << endl;
-	return 0;
+    int n, key;
+    cin >> n;
+    vector<int> A(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> A[i];
+    }
+    cin >> key;
+    int idx = binarySearch(A, key);
+    cout << idx << endl;
+    return 0;
 }
--- cppdsa/quickSort.cpp
+++ cppdsa/quickSort.cpp
@@ -2,32 +2,32 @@
 using namespace std;
 
 void quickSort(vector<int> &A, int start, int end) {
-	if (start >= end) return;
-	int pivot = A[end];
-	int i = start, j = end - 1;
-	while (i < j) {
-		while (i < j && A[i] <= pivot) i++;
-		while (i < j && A[j] > pivot) j--;
-		swap(A[i], A[j]);
-	}
-	if (A[i] > pivot)
-		swap(A[i], A[end]);
-	quickSort(A, start, i);
-	quickSort(A, i + 1, end);
+    if (start >= end) return;
+    int pivot = A[end];
+    int i = start, j = end - 1;
+    while (i < j) {
+        while (i < j && A[i] <= pivot) i++;
+        while (i < j && A[j] > pivot) j--;
+        swap(A[i], A[j]);
+    }
+    if (A[i] > pivot)
+        swap(A[i], A[end]);
+    quickSort(A, start, i);
+    quickSort(A, i + 1, end);
 }
 
 int main(int argc, char const *argv[])
 {
-	int n;
-	cin >> n;
-	vector<int> A(n);
-	for (int i = 0; i < n; ++i) {
-		cin >> A[i];
-	}
-	quickSort(A, 0, n - 1);
-	for (int i = 0; i < n; ++i) {
-		cout << A[i] << " ";
-	}
-	cout << endl;
-	return 0;
+    int n;
+    cin >> n;
+    vector<int> A(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> A[i];
+    }
+    quickSort(A, 0, n - 1);
+    for (int i = 0; i < n; ++i) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
